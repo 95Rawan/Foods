ae21445b461e95756619b30d339e899698740d3c
95Rawan <youlostme17@hotmail.com>
2019-08-16 22:45:39 +0300

    Finished post meal endpoint

---
+++ src/controllers/postMealForm.js
@@ -0,0 +1,33 @@
+const postMealData = require('./../database/queries/postMealData.js')
+const path = require('path')
+const fs = require('fs')
+const alert = require('alert-node')
+
+exports.getPostMealForm = (req, res) => {
+  const endpoint = req.originalUrl
+  const filePath = path.join(__dirname, `../views${endpoint}.hbs`)
+
+  fs.readFile(filePath, (err, file) => {
+    if (err) {
+      console.log(err)
+
+      res.writeHead(500, {
+        'Content-Type': 'text/html'
+      })
+      res.end('<h1>Server Error</h1>')
+    } else {
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      })
+      res.end(file)
+    }
+  })
+}
+
+exports.postMeal = (req, res) => {
+  const { foodname, country, img, description } = req.body
+  postMealData(foodname, country, img, description)
+    .then(() => alert('The meal was successfuly added'))
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
+}
